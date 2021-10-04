d317a2a194ac64dec5c845461ef484cfb0891e1c
Rajarshi <rajarshi@pitangent.com>
2021-10-04 18:43:39 +0000

    Updates

---
+++ _next/static/0FTWQl1ZMWzriOvQOqxDN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-4fd5ff81bca381f31068.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
